0c7c2aae8b785a3eb002937ff51ada38113ebd3c
AlejoOsorio <112147774+AlejoOsorio@users.noreply.github.com>
2025-08-06 16:19:16 -0500

    Addition to nvim-tree for better navigation

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,23 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- Here is defined the keys l and h, for open and close folders
+        vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+      end,
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+      },
+    }
   end,
 }
